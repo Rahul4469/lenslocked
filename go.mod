5419a666295521034b16b112ce91abc73ad9813b
Rahul Choudhary <rahulrc7692@gmail.com>
2025-08-27 17:35:03 +0530

    insalled chi to use as a router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/Rahul4469/new-project-go
+module github.com/Rahul4469/lenslocked
 
 go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.2.3 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Welcome to Go Web Server</h1>", 23231)
+	fmt.Fprintf(w, "<h1>Welcome to Go Web Server</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,19 +15,21 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>contact at</p><a href=\"rahul@gmail.com\">rahul@gmal.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
 
 func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contacr", contactHandler)
 
 	fmt.Println("Starting server at port 3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", nil)
 }
