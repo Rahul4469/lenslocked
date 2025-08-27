a3755e3b2a016e8d56a2164758231d0c58b4e472
Rahul Choudhary <rahulrc7692@gmail.com>
2025-08-27 19:37:38 +0530

    Started using chi(created router)

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,21 +17,27 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>contact at</p><a href=\"rahul@gmail.com\">rahul@gmal.com</a>.")
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h>FAQ Page</h>
+<ul>
+<li><b>Is there a fee version</b> Yes! We offer free Trail for 30 days on any plai plans.</li>
+</ul>
+<li><b>Is there a fee version</b> Yes! We offer free Trail for 30 days on any plai plans.</li>
+</ul>
+<li><b>Is there a fee version</b> Yes! We offer free Trail for 30 days on any plai plans.</li>
+</ul>	
+`)
+}
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contacr", contactHandler)
-
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting server at port 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
