279d6baf13a1b78dcedd9bc8965be026ed2a0eef
Rahul Choudhary <rahulrc7692@gmail.com>
2025-08-31 22:54:54 +0530

    validated parse

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,27 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Is there a free version",
+			Answer:   "Yes we offer a free trial for 30 days on any paid plans",
+		},
+		{
+			Question: "Is there a free version",
+			Answer:   "Yes we offer a free trial for 30 days on any paid plans",
+		},
+		{
+			Question: "Is there a free version",
+			Answer:   "Yes we offer a free trial for 30 days on any paid plans",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
